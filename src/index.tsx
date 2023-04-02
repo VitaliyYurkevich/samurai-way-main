acddaedac5b0ce31015bf3ab67a5615cad88a8f4
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-04-02 23:36:47 +0300

    upd

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 
-console.log('as')
+
 export let rerenderEntireTree = () => {
 
     ReactDOM.render(
