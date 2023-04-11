030243e6ca812a2e523734b577c52ee6d886e375
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-04-11 23:34:39 +0300

    upd

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import './index.css';
-import {store} from "./redux/state";
+import { store} from "./redux/state";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
+import {statePropsType, StorePropsType} from "./redux/state";
 
 
-export let rerenderEntireTree = () => {
+export let rerenderEntireTree = (state: statePropsType  ) => {
 
     ReactDOM.render(
         <BrowserRouter>
-            <App store={store} />
+            <App store={store}  />
         </BrowserRouter>, document.getElementById('root')
     )
 }
 
 
+rerenderEntireTree(store.getState())
+store.subscribe(()=>{
+    let state = store.getState()
+    rerenderEntireTree(state)
+})
+
+
+/*
 store.subscribe(rerenderEntireTree)
-rerenderEntireTree()
+rerenderEntireTree()*/
--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -2,10 +2,16 @@ import {combineReducers, createStore} from "redux";
 import ProfileReducer from "./profile-reducer";
 import DialogsReducer from "./dialogs-reducer";
 
+/*type RootReducerType = typeof reducers
+типизация заглушка???
+export type AppType = ReturnType<RootReducerType>*/
+
 let reducers = combineReducers({
-    ProfileReducer,
-    DialogsReducer
+    profilePage: ProfileReducer,
+    messagesPage: DialogsReducer
 })
+
+
 
 export let store = createStore(reducers)
 
