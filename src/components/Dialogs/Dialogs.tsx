c39df5293a3c22c64474b5353fe06204bfcdd08f
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-04-17 00:35:58 +0300

    upd

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,6 +4,7 @@ import DialogsItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import {DialogsPageType} from "../../redux/dialogs-reducer";
 import {DialogsPropsType} from "./DialogsContainer";
+import {v1} from "uuid";
 
 /*type DialogsPropsType = {
     newMessageBody: (body: string) => void
@@ -14,20 +15,23 @@ import {DialogsPropsType} from "./DialogsContainer";
 
 function Dialogs(props: DialogsPropsType) {
 
-
-    let dialogsElements = props.messagePage.dialogData.map(d => <DialogsItem name={d.name} id={d.id}/>)
-    let messagesElements = props.messagePage.messageData.map((m) => {
+    let newMessageBody = props.messagePage.newMessageBody
+    let dialogsElements = props.messagePage.dialogs.map(d => <DialogsItem name={d.name} id={v1()}/>)
+    let messagesElements = props.messagePage.messages.map((m) => {
         return (
             <Message message={m.message}/>
         )
     })
-    let newMessageBody = props.messagePage.newMessageBody
+
     let onSendMessageClick = () => {
         props.sendMessage()
     }
     let onNewMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        let body = e.currentTarget.value
-        props.updateNewMessageBody(body)
+        //let body = e.currentTarget.value
+        //props.updateNewMessageBody(body)
+
+        newMessageBody = e.currentTarget.value
+        props.updateNewMessageBody(newMessageBody)
     }
 
     return (
