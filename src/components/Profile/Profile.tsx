a55da4efd44be2ed942b6047c3dd12efd52e586f
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-04-16 21:29:43 +0300

    upd

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import classes from './Profile.module.css';
-import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import {store} from "../../redux/redux-store";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
 
@@ -13,7 +10,7 @@ function Profile() {
     return (
         <div>
             <ProfileInfo/>
-            <MyPostsContainer    />  {/*dispatch={store.dispatch.bind(store)}
+            <MyPostsContainer/>  {/*dispatch={store.dispatch.bind(store)}
                      updateNewPostText={store.updateNewPostText.bind(store)}
                      newPostText={store._state.profilePage.newPostText}
                 //addPost={store.addPost.bind(store)}
