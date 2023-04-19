762884b36e89ad389fc1db296da1f85883006fc6
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-04-19 23:29:37 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ const App = () => {
                     <Route path={'/news'} render={() => <News/>}/>
                     <Route path={'/music'} render={() => <Music/>}/>
                     <Route path={'/settings'} render={() => <Settings/>}/>
+                    <Route path={'/users'} render={() => <div> users</div>}/>
                 </div>
             </div>
         </BrowserRouter>
