087e7e9d64f0a6917ffb2ae687206cc82cfd8ed8
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-04-20 00:48:27 +0300

    upd

---
+++ src/components/Users/UsersContainer.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {connect} from "react-redux";
+import Users from "./Users";
+import {AppStateType} from "../../redux/redux-store";
+import {Dispatch} from "redux";
+import {followAC, setUsersAC, unFollowAC, usersType} from "../../redux/users-reducer";
+
+
+let mapStateToProps = (state: AppStateType) => {
+    return {
+        usersPage: state.usersPage
+    }
+}
+
+let mapDispatchToProps = (dispatch: Dispatch):MapDispatchPropsType => {
+    return {
+        follow: (userId) => {
+            dispatch(followAC(userId))
+        },
+        unFollow: (userId) => {
+            dispatch(unFollowAC(userId))
+        },
+        setUsers: (users) => {
+            dispatch(setUsersAC(users))
+        }
+
+    }
+}
+
+type MapDispatchPropsType = {
+    follow: (userId: string) => void
+    unFollow: (userId: string) => void
+    setUsers: (users: Array<usersType>) => void
+}
+
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+
+export default UsersContainer
