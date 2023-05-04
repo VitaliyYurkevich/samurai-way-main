1a5975fed8a4ba844ecb2da9023c923e86f5f5e7
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-05-04 23:45:07 +0300

    upd

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 import {Provider} from "react-redux";
-
+console.log('')
 
 
 {/*dispatch={store.dispatch.bind(store)}*/}
