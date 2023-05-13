d27bcf9c3286b20c7ac3700e02873ee4c4253b0e
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-05-13 23:34:57 +0300

    upd

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import classes from "./ProfileInfo.module.css"
+import {ProfilePageType, profileType} from "../../../redux/profile-reducer";
+import Preloader from "../../common/Preloader";
 
+type ProfileInfoPropsType = {
+    profile: profileType
+}
 
-function ProfileInfo() {
+function ProfileInfo(props: ProfileInfoPropsType) {
+if(!props.profile){
+    return <Preloader />
+}
+debugger
     return (
         <div className={classes.content}>
             <div>
@@ -10,6 +19,7 @@ function ProfileInfo() {
                     src='https://img1.goodfon.ru/wallpaper/nbig/6/f2/priroda-kanada-gory-ozero-domik-zima-sneg.jpg'/>
             </div>
             <div className={classes.descriptionBlock}>
+                <img src={props.profile.photos.large}/>
                 ava + description
             </div>
         </div>
