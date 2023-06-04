d537bd52e9cbe1b5b2a30855560e48d99c03d112
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-06-04 21:10:32 +0300

    upd

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import classes from './Header.module.css'
+import {NavLink} from "react-router-dom";
+
+export type HeaderPropsType = {
+
+}
 
 function Header() {
     return <header className ={classes.header}>
         <img
             src='https://avatars.mds.yandex.net/i?id=3cd27a746e4373e70f6167feca888b83fad687f8-7761916-images-thumbs&n=13'/>
+        <div className={classes.loginBlock}>
+            <NavLink to={'/login'}>LOGIN</NavLink>
+        </div>
     </header>
 }
 
