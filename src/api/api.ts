b4a91bc412174a685b0c8b8314c03f0940861157
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-06-05 00:44:14 +0300

    upd

---
+++ src/api/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    headers: {
+        "API-KEY": "482cc18a-19a0-4ab1-be65-64dee4acdd40"
+    }
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10 ){
+        return  instance.get(`users?page=${currentPage}&count=${pageSize}`, {
+            withCredentials: true
+        }).then(response => {
+                return   response.data
+            }
+
+        )
+    }
+}
+
+
+
