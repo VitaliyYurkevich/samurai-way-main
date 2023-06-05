82f52f6925423f4c5a85ea198f7ab20d80b40f5e
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-06-05 21:06:04 +0300

    upd

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import classes from './Header.module.css'
 import {NavLink} from "react-router-dom";
 
-export type HeaderPropsType = {
+type HeaderPropsType = {
+    isAuth: boolean
+    login: null
 
 }
 
-function Header() {
+function Header(props: HeaderPropsType) {
     return <header className ={classes.header}>
         <img
             src='https://avatars.mds.yandex.net/i?id=3cd27a746e4373e70f6167feca888b83fad687f8-7761916-images-thumbs&n=13'/>
         <div className={classes.loginBlock}>
-            <NavLink to={'/login'}>LOGIN</NavLink>
+            {props.isAuth ? props.login :
+                <NavLink to={'/login'}>LOGIN</NavLink>
+            }
+
         </div>
     </header>
 }
