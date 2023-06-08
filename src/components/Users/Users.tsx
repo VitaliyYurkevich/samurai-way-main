a8f5b709a4ef3332ab1f3c01ec8c31f4a8ee0cfa
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-06-08 20:42:05 +0300

    upd

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from "./users.module.css";
-import {usersType} from "../../redux/users-reducer";
+import {followingProgressType, toggleFollowingProgressAC, usersType} from "../../redux/users-reducer";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 
 export type UsersPropsType = {
@@ -12,51 +13,81 @@ export type UsersPropsType = {
     currentPage: number
     onPageChanged: (pageNumber: number) => void
     users: Array<usersType>
+    toggleFollowingProgress: (isFetching: boolean, userId: number ) => void
+    followingInProgress: number[]
 }
 
 const Users = (props: UsersPropsType) => {
-
+    let userPhoto = 'https://img.freepik.com/premium-vector/male-avatar-icon-unknown-or-anonymous-person-default-avatar-profile-icon-social-media-user-business-man-man-profile-silhouette-isolated-on-white-background-vector-illustration_735449-120.jpg'
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
     let pages: Array<number> = []
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
-        return (
+    return (
+        <div>
             <div>
                 <div>
-                    <div>
-                        {pages.map(p => {
-                            return (
-                                // @ts-ignore
-                                <span className={props.currentPage === p && styles.selectedPage}
-                                      onClick={(e) => {
-                                          props.onPageChanged(p)
-                                      }}
-                                >{p}</span>
-                            )
+                    {pages.map(p => {
+                        return (
+                            // @ts-ignore
+                            <span className={props.currentPage === p && styles.selectedPage}
+                                  onClick={(e) => {
+                                      props.onPageChanged(p)
+                                  }}
+                            >{p}</span>
+                        )
 
-                        })}
-                    </div>
-                    {/* <button onClick={this.getUsers}>Get users</button>*/}
-                    <div></div>
-                    {
-                        props.users.map(u => {
+                    })}
+                </div>
+                {/* <button onClick={this.getUsers}>Get users</button>*/}
+                <div></div>
+                {
+                    props.users.map(u => {
+
+
                             return (
                                 <div key={u.id}>
                     <span>
                         <div>
                             <NavLink to={'/profile/' + u.id}>
-                                <img src={u.photos != null ? u.photos : '@'} className={styles.userPhoto}/>
+                                <img src={u.photos !== null ? userPhoto : '@'} className={styles.userPhoto}/>
                             </NavLink>
 
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => {
+                                ? <button disabled={props.followingInProgress.some(id => id === Number(u.id))} onClick={() => {
+                                    props.toggleFollowingProgress(true, Number(u.id))
+                                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                        withCredentials: true,
+                                        headers: {
+                                            "API-KEY": "482cc18a-19a0-4ab1-be65-64dee4acdd40"
+                                        }
+                                    })
+                                        .then(response => {
+                                            if (response.data.resultCode === 0) {
+                                                props.follow(u.id)
+                                            }
+                                            props.toggleFollowingProgress(false, Number(u.id))
+                                        })
                                     props.follow(u.id)
                                 }}>Follow</button>
-                                : <button onClick={() => {
-                                    props.unFollow(u.id)
+
+                                : <button disabled={props.followingInProgress.some(id => id === Number(u.id))} onClick={() => {
+                                    props.toggleFollowingProgress(true, Number(u.id))
+                                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                        withCredentials: true,
+                                        headers: {
+                                            "API-KEY": "482cc18a-19a0-4ab1-be65-64dee4acdd40"
+                                        }
+                                    })
+                                        .then(response => {
+                                            if (response.data.resultCode === 1) {
+                                                props.unFollow(u.id)
+                                            }
+                                            props.toggleFollowingProgress(false, Number(u.id))
+                                        })
                                 }}>UnFollow</button>
                             }
                         </div>
@@ -74,14 +105,12 @@ const Users = (props: UsersPropsType) => {
                                 </div>
                             )
                         }
-                        )
-                    }
-                </div>
+                    )
+                }
             </div>
-        );
-    }
-
-
+        </div>
+    );
+}
 
 
 export default Users;
