5e7fd94d29281db16ac10715cd615ab9358c5181
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-06-12 00:19:39 +0300

    upd

--- src/api/api.ts
+++ src/api/api.ts
@@ -10,14 +10,19 @@ const instance = axios.create({
 })
 
 export const usersAPI = {
-    getUsers(currentPage = 1, pageSize = 10 ){
-        return  instance.get(`users?page=${currentPage}&count=${pageSize}`, {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`, {
             withCredentials: true
         }).then(response => {
-                return   response.data
+                return response.data
             }
-
         )
+    },
+    follow(userId = 1) {
+        instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {})
+    },
+    unfollow(userId = 1) {
+     return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
     }
 }
 
