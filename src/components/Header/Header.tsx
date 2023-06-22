1ce699ee23d6dc7e539d6561ae22941c14411c84
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-06-22 23:52:39 +0300

    upd

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,6 +9,7 @@ type HeaderPropsType = {
 }
 
 function Header(props: HeaderPropsType) {
+    debugger
     return <header className ={classes.header}>
         <img
             src='https://avatars.mds.yandex.net/i?id=3cd27a746e4373e70f6167feca888b83fad687f8-7761916-images-thumbs&n=13'/>
