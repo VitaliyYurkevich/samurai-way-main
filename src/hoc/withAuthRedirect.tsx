503fab6098605c632950342cdd0c41b947252252
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-06-26 23:55:40 +0300

    upd

---
+++ src/hoc/withAuthRedirect.tsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import {Redirect} from "react-router-dom";
+
+export const withAuthRedirect = (Component: any) => {
+
+
+
+    class RedirectComponent extends React.Component<any, any> {
+        render() {
+            if (!this.props.isAuth) return <Redirect to='/login'/>
+
+                return <Component {...this.props}/>
+        }
+    }
+    return RedirectComponent
+}
