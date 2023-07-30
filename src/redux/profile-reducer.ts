acad64f2c832b8e6b0f98ca12b58b0f91fdca4b2
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-07-30 23:02:12 +0300

    upd

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -3,11 +3,12 @@ import {ActionDispatchTypes} from "./redux-store";
 import post from "../components/Profile/MyPosts/Post/Post";
 import {v1} from "uuid";
 import {Dispatch} from "redux";
-import {usersAPI} from "../api/api";
+import {profileAPI, usersAPI} from "../api/api";
 
 export const ADD_POST = "ADD_POST"
 export const UPDATE_NEW_POST_TEXT = "UPDATE_NEW_POST_TEXT"
 export const SET_USER_PROFILE = "SET_USER_PROFILE"
+export const SET_STATUS = "SET_STATUS"
 
 export type setUserProfileType = {
     type: typeof SET_USER_PROFILE
@@ -21,6 +22,11 @@ export type addPostType = {
 export type updateNewPostTextType = {
     type: typeof UPDATE_NEW_POST_TEXT,
     newPost: string
+}
+
+export type setStatusType = {
+    type: typeof SET_STATUS
+    status: any
 }
 
 export const addPostAC = () => {
@@ -41,9 +47,30 @@ export const setUserProfileAC = (profile: string) => {
         profile: profile
     } as const
 }
+export const setStatusAC = (status: any) => {
+    return {
+        type: SET_STATUS,
+        status: status
+    } as const
+}
 export const getUserProfileTC = (userId: number) => (dispatch: Dispatch) => {
     usersAPI.getProfile(userId).then( response => {
             dispatch(setUserProfileAC(response.data))
+        }
+    )
+}
+export const getStatusTC = (userId: number) => (dispatch: Dispatch) => {
+    profileAPI.getStatus(userId).then( response => {
+        dispatch(setStatusAC(response.data))
+    }
+    )
+}
+export const updateStatusTC = (status: any) => (dispatch: Dispatch) => {
+    profileAPI.updateStatus(status).then( response => {
+        if (response.data.resultCode === 0) {
+            dispatch(setStatusAC(response.data))
+        }
+
         }
     )
 }
@@ -53,7 +80,8 @@ const initialState = {
         {id: 1, message: 'Hi, how are you?', likesCount: 2}
     ],
     newPostText: '',
-    profile: null
+    profile: null,
+    status: ''
 }
 
 export type profileType = {
@@ -81,6 +109,11 @@ const ProfileReducer = (state: ProfilePageType = initialState, action: ActionDis
             return {
                 ...state,
                 newPostText: action.newPost
+            }
+        case SET_STATUS:
+            return {
+                ...state,
+                status: action.status
             }
         case SET_USER_PROFILE:
             return {
