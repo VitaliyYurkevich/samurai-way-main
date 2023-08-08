bb69e26ef99fe7d76538fc9d73b640ad6b2d1e94
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-08-08 23:41:02 +0300

    upd

--- src/api/api.ts
+++ src/api/api.ts
@@ -47,6 +47,12 @@ return instance.get(`profile/status/` + userId)
 export const authAPI = {
     me () {
         return instance.get(`auth/me`, )
+    },
+    login (email: string, password: string, rememberMe = false) {
+        return instance.post(`auth/login`, {email, password, rememberMe} )
+    },
+    logout () {
+        return instance.delete(`auth/login`)
     }
 }
 
