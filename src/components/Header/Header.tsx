674a38e2e5a4d5a3def5326140b9c3e1f3581142
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-08-11 19:30:50 +0300

    upd

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import classes from './Header.module.css'
 import {NavLink} from "react-router-dom";
-import {logout} from "../../redux/auth-reducer";
 
 type HeaderPropsType = {
     isAuth: boolean
     login: null
-
+    logout: () => void
 }
 
 function Header(props: HeaderPropsType) {
@@ -16,7 +15,7 @@ function Header(props: HeaderPropsType) {
             src='https://avatars.mds.yandex.net/i?id=3cd27a746e4373e70f6167feca888b83fad687f8-7761916-images-thumbs&n=13'/>
         <div className={classes.loginBlock}>
             {props.isAuth ?
-                <div>{props.login} - <button onClick={logout}>Log out</button></div>
+                <div>{props.login} - <button onClick={props.logout}>Log out</button></div>
                 : <NavLink to={'/login'}>LOGIN</NavLink>
             }
 
