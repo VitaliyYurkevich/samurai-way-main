6869b95b16aabb4fa6c7291ae213ee9bc159316d
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-08-11 19:30:56 +0300

    upd

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 import axios from "axios";
 import {connect} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
-import {getAuthUserDataTC, setAuthUserDataAC} from "../../redux/auth-reducer";
+import {getAuthUserData, logout, setAuthUserDataAC} from "../../redux/auth-reducer";
 import {authAPI} from "../../api/api";
 
 type MapStatePropsType = {
@@ -12,7 +12,8 @@ type MapStatePropsType = {
 }
 
 type MapDispatchPropsType = {
-    getAuthUserDataTC: () => void
+    getAuthUserData: () => void
+    logout: () => void
 }
 
 type OwnPropsType = MapStatePropsType & MapDispatchPropsType
@@ -21,12 +22,12 @@ type OwnPropsType = MapStatePropsType & MapDispatchPropsType
 
 class HeaderContainer extends React.Component<OwnPropsType> {
     componentDidMount() {
-        this.props.getAuthUserDataTC()
+        this.props.getAuthUserData()
     }
 
     render() {
         return(
-            <Header  isAuth={this.props.isAuth} login={this.props.login}/>
+            <Header {...this.props} isAuth={this.props.isAuth} login={this.props.login}/>
         );
     }
 }
@@ -34,14 +35,15 @@ class HeaderContainer extends React.Component<OwnPropsType> {
 const mapStateToProps = (state: AppStateType):MapStatePropsType => {
 
     return{
-        // @ts-ignore
-        isAuth: state.auth.isAuth,
-        // @ts-ignore
-        login: state.auth.login
+
+        isAuth: state.auth["isAuth"],
+
+        login: state.auth["login"]
     }
 }
 
 export default connect(mapStateToProps, {
+    logout,
     setAuthUserDataAC,
-    getAuthUserDataTC
+    getAuthUserData
 } )(HeaderContainer)
