db0d2ff634c4cddf573b8022d87487887f06dadb
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-08-16 23:56:45 +0300

    upd

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+
+
+export const ProfileStatusWithHooks = (props: any) => {
+
+    let stateWithSetState = useState(false)
+    let editMode = stateWithSetState[0]
+    let setEditMode = stateWithSetState[1]
+
+    return (
+        <div>
+            {
+                !editMode &&
+                <div>
+                    <span onDoubleClick={() => {
+                    }}>{props.status || '------'}</span>
+                </div>
+            }
+            {
+                editMode &&
+                <div>
+                    <input onChange={() => {
+                    }} onBlur={() => {
+                    }}
+                           value={''}/>
+                </div>
+            }
+        </div>
+    )
+}
