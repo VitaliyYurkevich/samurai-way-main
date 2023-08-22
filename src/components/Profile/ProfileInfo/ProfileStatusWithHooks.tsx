8cbe189581016ad7d2f3e8b6b88879605d326f32
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-08-22 23:56:08 +0300

    upd

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -5,28 +5,31 @@ import {log} from "util";
 export const ProfileStatusWithHooks = (props: any) => {
 
     let [editMode, setEditMode] = useState(false)
-const activateMode = () => {
+    const activateMode = () => {
         setEditMode(true)
-}
+    }
+    const deactivateEditMode = () => {
+        setEditMode(false)
+    }
 
-    return (
-        <div>
-            {
-                !editMode &&
-                <div>
+return (
+    <div>
+        {
+            !editMode &&
+            <div>
                     <span onDoubleClick={activateMode}>
                         {props.status || '------'}</span>
-                </div>
-            }
-            {
-                editMode &&
-                <div>
-                    <input onChange={() => {
-                    }} onBlur={() => {
-                    }}
-                           value={''}/>
-                </div>
-            }
-        </div>
-    )
+            </div>
+        }
+        {
+            editMode &&
+            <div>
+                <input onChange={() => {
+                }} onBlur={() => {
+                }}
+                       value={''}/>
+            </div>
+        }
+    </div>
+)
 }
